86e477704edf98721050403826f931097c270322
memochou1993 <memochou1993@users.noreply.github.com>
2022-11-25 20:20:18 +0000

    deploy: a265b3084279e9ae6d9c79d900010c4b81ccd2b3

--- _next/static/oYfHObYSN67_tI4-CPsyF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2e70e6ed9581cd6a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
