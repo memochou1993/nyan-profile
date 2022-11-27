118eca425dc2d89a4c967b17ae88186f5e4dfcf7
memochou1993 <memochou1993@users.noreply.github.com>
2022-11-27 07:11:14 +0000

    deploy: 58f5b0585e4991b68e94e7439b4e6cbe7303d9a7

--- _next/static/YSgm8fH42-cxZCyGWQG9Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-aacc65d2fb6daaa6.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/_Dix9u8lRknrgMVyDDh77/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e75806de154c5acb.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
